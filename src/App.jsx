8e96dac6bcec1eb0a08099294aa8fa986acf75a2
Bojan <bojan@Bojans-MacBook-Air.local>
2024-03-26 23:58:15 +0100

    Instaling packets and frameworks

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+
+  return (
+    <div className="">
+      <h1 className="bg-[#f90] flex justify-center font-sans text-3xl">WELCOME TO WEB PHONE MARKET</h1>
+    </div>
+  )
+}
+
+export default App
