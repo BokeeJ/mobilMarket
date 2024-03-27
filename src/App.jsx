13a1bd2b14d84a499218784bfe64d58a114ee046
Bojan <bojan@Bojans-MacBook-Air.local>
2024-03-27 22:50:30 +0100

    import mp4 video spot on bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar";
+import { VscUnmute } from "react-icons/vsc";
+import { FaVolumeMute } from "react-icons/fa";
+
 
 function App() {
+  const [sound, setSound] = useState(false);
 
+  const handleSound = () => {
+    setSound(!sound);
+  };
 
   return (
-    <div className="">
-      <h1 className="bg-[#f90] flex justify-center font-sans text-3xl">WELCOME TO WEB PHONE MARKET</h1>
+    <div className="relative">
+      <button onClick={handleSound} className="text-white text-3xl bg-black absolute top-4 right-4 z-50">{sound ? <VscUnmute color="white" /> : <FaVolumeMute />}</button>
+      <div className="relative">
+        <video loop autoPlay className="absolute inset-0 w-full h-full object-cover opacity-50" muted={!sound}>
+          <source src="/1.mp4" type="video/mp4" />
+
+        </video>
+        <div className="relative z-10">
+          <Navbar />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='h-[700px]  '>
+            <div className='justify-center flex '>
+                <h1 className='justify-center items-center flex fixed text-6xl mt-20 font-bebas '>SFRJ GROOVE BEND</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
