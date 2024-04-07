afdee2b424dd868ff086e162de2a3754a1826a27
Bojan <bojan@Bojans-MacBook-Air.local>
2024-04-07 23:37:21 +0200

    New NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import React, { useState } from "react";
 import Navbar from "./Components/Navbar";
-import { VscUnmute } from "react-icons/vsc";
-import { FaVolumeMute } from "react-icons/fa";
-import Slider from "./Components/Slider";
-
-
 function App() {
-  const [sound, setSound] = useState(false);
 
-  const handleSound = () => {
-    setSound(!sound);
-  };
+
 
   return (
-    <div className="relative">
-      <button onClick={handleSound} className="text-white text-3xl bg-black absolute top-4 right-4 z-50">{sound ? <VscUnmute color="white" /> : <FaVolumeMute />}</button>
-      <div className="relative">
-        <video loop autoPlay className="absolute inset-0 w-full h-full object-cover opacity-50" muted={!sound}>
-          <source src="/1.mp4" type="video/mp4" />
-
-        </video>
-        <div className="relative z-10">
-          <Navbar />
-          <div className="flex justify-center">
-            <h1 className="text-black text-4xl">CHOOSE YOUR FAVOURITE</h1></div>
-          <Slider />
-        </div>
-      </div>
-
+    <div>
+      <Navbar />
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
+// react icons
+import { CiMail } from "react-icons/ci";
+import { FaPhoneAlt } from "react-icons/fa";
+import { CiHeart } from "react-icons/ci";
+import { IoIosSearch } from "react-icons/io";
+import { FaRegUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 function Navbar() {
     return (
-        <div className='h-[700px]  '>
-            <div className='justify-center flex '>
-                <h1 className='justify-center items-center flex fixed text-6xl mt-20 font-bebas '>SFRJ GROOVE BEND</h1>
+        <div className='bg-[#20285d]'>
+            {/* miniNavbar */}
+            <div className='flex justify-between w-full  text-white'>
+                <ul className='flex gap-4 m-5 lg:ml-16 '>
+                    <FaPhoneAlt size={25} />
+                    <li>Pozovite nas</li>
+                    <CiMail size={25} />
+                    <li>Prodaja@gmail.com</li>
+
+                </ul>
+                <ul className='flex gap-3 m-5 lg:mr-16'>
+                    <CiHeart size={25} />
+                    <li> Lista zelja</li>
+
+                </ul>
+            </div>
+            {/* mainNavbar */}
+            <div>
+                <div className='flex items-center relative w-full'>
+                    <img src="/1.png" alt="Pc Shop" className='w-[200px] h-[80px] m-5 lg:ml-16 rounded-lg' />
+                    <div className='relative w-[800px]'>
+                        <input type="text" placeholder='Unesite pojam za pretragu...' className='w-[800px] h-[40px] rounded-full p-5' />
+                        <IoIosSearch color='black' onClick={() => { console.log('CaoZeki') }} size={25} className='absolute right-0 mr-5 top-1/2 transform -translate-y-1/2' />
+                    </div>
+                    <div className=' items-center flex gap-6'>
+                        <ul className='flex items-center gap-2 text-white ml-16'>
+                            <FaRegUser size={25} />
+                            <SignedOut>
+                                <SignInButton />
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton afterSignOutUrl='/' />
+                            </SignedIn>
+                        </ul>
+
+                        <ul className='flex items-center gap-2 text-white '>
+                            <FaShoppingCart size={25} />
+                            <li>0</li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
