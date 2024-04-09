14dc3b0bb5a26dd161429584bfea00e8d1f885cd
Bojan <bojan@Bojans-MacBook-Air.local>
2024-04-09 23:24:16 +0200

    products css

--- src/Components/Proizvodi.jsx
+++ src/Components/Proizvodi.jsx
@@ -5,19 +5,26 @@ function Proizvodi() {
     const [proizvodi, setProizvodi] = useState([])
     useEffect(() => {
         ProductsService.getAllProducts()
-            .then((products) => setProizvodi(products.data.products))
-            .catch((error) => console.log('error'));
-    }, []); // Dodali smo prazan niz zavisnosti da bismo osigurali da se efekat pokrene samo jednom
+            .then((products) => {
+                setProizvodi(products.data.products);
+                toast.success('Podaci su uspešno preuzeti!');
+            })
+            .catch((error) => {
+                console.error('Greška prilikom preuzimanja podataka:', error);
+                toast.error('Došlo je do greške prilikom preuzimanja podataka.');
+            });
+    }, []);
 
     return (
-        <div className='flex flex-wrap items-center justify-center' >
+        <div className='flex flex-wrap items-center justify-center w-[70%] h-full mx-auto gap-2 shadow-2xl m-5' >
 
             {proizvodi.map((product) => {
                 return (
-                    <div key={product.id} className='items-center w-[200px] h-[300px]'>
-                        <h1>{product.name}</h1>
-                        <img src={product.thumbnail} className='w-[100px] w-[120px]' alt={product.name} />
-                        <p>{product.description}</p>
+                    <div key={product.id} className='w-[200px] h-[200px] border-black border-2 m-2 text-center rounded-lg truncate'>
+                        <img src={product.thumbnail} className='w-full h-[100px] object-cover overhide truncate' alt={product.name} />
+                        <h1 className='p-2'>{product.title}</h1>
+                        <hr className='w-[70%]  border-black mx-auto' />
+                        <p className='text-[#751f1f] p-5'>{product.price}$</p>
                     </div>
                 )
             })}
